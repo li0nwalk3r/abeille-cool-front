355972cfb6fb9f2454a5804ce3e029bdffa7c1b0
louisforcade <louis_forcade@hotmail.fr>
2019-04-17 17:34:24 +0200

    Page articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ArticleComponent} from './article/article.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'article', component: ArticleComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+

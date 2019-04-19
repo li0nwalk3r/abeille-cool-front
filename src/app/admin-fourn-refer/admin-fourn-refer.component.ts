d8035eeacb955d6b0a6b2988c3be48924bf42ea8
Jordi <jordisarpoulet@hotmail.com>
2019-04-19 11:47:39 +0200

    Bouton Supprimer dans Admin Fournisseur Références

--- src/app/admin-fourn-refer/admin-fourn-refer.component.ts
+++ src/app/admin-fourn-refer/admin-fourn-refer.component.ts
@@ -65,4 +65,9 @@ export class AdminFournReferComponent implements OnInit {
     this.produitList.valide = false;
     this.save();
   }
+
+  remove(id: number) {
+      this.adminFournReferService.delete(id).subscribe(resp => this.listProduitByFournisseur(),
+        err => console.log(err));
+    }
 }

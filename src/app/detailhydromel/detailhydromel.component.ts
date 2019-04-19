21d615e8f46b21a7b3aa78dc4f19a2dc303ae986
caumes <caumes.geraldine@gmail.com>
2019-04-19 11:06:12 +0200

    modifs commandeclient.ts commandeclienthhtpservice lignecommandehhtpservice detailhydromel

--- src/app/detailhydromel/detailhydromel.component.ts
+++ src/app/detailhydromel/detailhydromel.component.ts
@@ -3,6 +3,12 @@ import {DetailhydromelHttpService} from './detailhydromel-http.service';
 import {Article} from '../model/article';
 import {ActivatedRoute} from '@angular/router';
 import {Avis} from '../model/avis';
+import {CommandeClientHttpService} from '../formulaire-commande-client/commande-client-http.service';
+import {CommandeClient} from '../model/commande-client';
+import {LigneCommande} from '../model/ligne-commande';
+import {ClientHttpService} from '../formulaire-commande-client/client-http.service';
+import {Client} from '../model/client';
+import {LigneCommandeHttpService} from '../facturation/ligne-commande-http.service';
 
 @Component({
   selector: 'app-detailhydromel',
@@ -11,10 +17,17 @@ import {Avis} from '../model/avis';
 })
 export class DetailhydromelComponent implements OnInit {
   hydromel: Article = null;
-  qte:number;
-  listeAvis:Array<Avis> = new Array<Avis>();
+  qte: number;
+  listeAvis: Array<Avis> = new Array<Avis>();
+  clientId: number;
+  listeCommandeClient: Array<CommandeClient> = new Array<CommandeClient>();
+  ligneCommande: LigneCommande;
+  client: Client;
+  commandeClient: CommandeClient;
 
-  constructor(private route: ActivatedRoute, private detailHydromelService: DetailhydromelHttpService) {
+  constructor(private route: ActivatedRoute, private clientHttpService: ClientHttpService,
+              private detailHydromelService: DetailhydromelHttpService,
+              private commandeClientService: CommandeClientHttpService, private ligneCommandeService: LigneCommandeHttpService) {
     this.route.params.subscribe(parameters => this.find(parameters.id));
   }
 
@@ -29,15 +42,61 @@ export class DetailhydromelComponent implements OnInit {
   }
 
   createLigne(article: Article, qte: number) {
+    if(sessionStorage.getItem("type")=="CLIENT"){
+           this.clientId=Number(sessionStorage.getItem("type_id"));
+           console.log("clientid"+this.clientId)
+           console.log(sessionStorage.getItem("type_id"));
+          console.log("hello");
+        }
+    this.detailHydromelService.create(article, qte).subscribe(resp => {
+      this.ligneCommande = resp;
+      this.commandeClientService.findPanier(this.clientId).subscribe(resp => {
+          this.listeCommandeClient = resp;
+          if (this.listeCommandeClient.length == 0) {
+            this.clientHttpService.findById(this.clientId).subscribe(resp => {
+              this.client = resp;
+              //  });
+              this.commandeClient=new CommandeClient(null, null, 'Panier', [], this.client)
+              this.commandeClientService.saveSubscribeOutside(this.commandeClient).subscribe(resp => {
+                this.commandeClientService.load();
+                this.commandeClient=resp;
+                this.ligneCommande.commandeClient=this.commandeClient;
+                this.ligneCommandeService.save(this.ligneCommande);
+                sessionStorage.setItem("commande_id",String(this.commandeClient.id));
 
-    this.detailHydromelService.create(article, qte).subscribe();
-  }
+              });
+              // this.commandeClientService.save(this.commandeClient).subscribe(;
+              //this.ligneCommande.commandeClient=this.commandeClient;
+              //this.ligneCommandeService.save(this.ligneCommande);
+            });
+            } else {
 
-  findAvis(article: Article){
-    console.log("findAvis()");
-     this.detailHydromelService.findAvis(article).subscribe( resp=>{
-       console.log("findAvis() in subscribe");
-       this.listeAvis = resp;
-     })
+            this.ligneCommande.commandeClient = this.listeCommandeClient[0];
+            this.ligneCommandeService.save(this.ligneCommande);
+            sessionStorage.setItem("commande_id",String(this.ligneCommande.commandeClient.id));
+            console.log(this.ligneCommande.commandeClient.id);
+            this.listeCommandeClient[0].client = this.client;
+            if(this.listeCommandeClient[0].lignesCommande==null){
+              this.listeCommandeClient[0].lignesCommande = new Array<LigneCommande>();
+            }
+            this.listeCommandeClient[0].lignesCommande.push(this.ligneCommande);
+
+          }});
+        });
+    };
+
+    //,this.listeCommandeClient=null);
+   // console.log(this.listeCommandeClient);
+
+
+    //this.detailHydromelService.create(article, qte).subscribe();
+ // }
+
+  findAvis(article: Article) {
+    console.log('findAvis()');
+    this.detailHydromelService.findAvis(article).subscribe(resp => {
+      console.log('findAvis() in subscribe');
+      this.listeAvis = resp;
+    });
   }
 }
